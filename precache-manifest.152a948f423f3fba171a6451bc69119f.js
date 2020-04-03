33281ce4730ac3fc8a4e763e328ec2ae14b8ad08
Martin Foakes <foakes.m@gmail.com>
2020-04-03 18:28:11 +0200

    Updates

---
+++ precache-manifest.152a948f423f3fba171a6451bc69119f.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bf2816417b06f32d47a6378740824d",
+    "url": "/audio-hardware-archive/index.html"
+  },
+  {
+    "revision": "f3ab58a65a2f096be122",
+    "url": "/audio-hardware-archive/static/js/2.01e64527.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/audio-hardware-archive/static/js/2.01e64527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70e012cde9b4afa1dc0",
+    "url": "/audio-hardware-archive/static/js/main.4d571519.chunk.js"
+  },
+  {
+    "revision": "f6cb8402f1e72d9a9b76",
+    "url": "/audio-hardware-archive/static/js/runtime-main.b830c127.js"
+  },
+  {
+    "revision": "550081c259c908a22934c882a24bcdd4",
+    "url": "/audio-hardware-archive/static/media/BD0.550081c2.WAV"
+  },
+  {
+    "revision": "edf3c9985a950e0a31b1f51b6c6a93f5",
+    "url": "/audio-hardware-archive/static/media/BassD.edf3c998.wav"
+  },
+  {
+    "revision": "7610b572bfef54e4cf761e42d402c331",
+    "url": "/audio-hardware-archive/static/media/COW.7610b572.WAV"
+  },
+  {
+    "revision": "68ea07ebaed839019d70a6ee41e69886",
+    "url": "/audio-hardware-archive/static/media/CRS.68ea07eb.WAV"
+  },
+  {
+    "revision": "acce3dfece1276a2249756b8af045023",
+    "url": "/audio-hardware-archive/static/media/Clap.acce3dfe.wav"
+  },
+  {
+    "revision": "4072bfad378be3fc3bfea0916ba9cbc6",
+    "url": "/audio-hardware-archive/static/media/CowB.4072bfad.wav"
+  },
+  {
+    "revision": "0fb15f6b5b56b1db06e6db3c53be56f6",
+    "url": "/audio-hardware-archive/static/media/Crash.0fb15f6b.wav"
+  },
+  {
+    "revision": "0216b4f4cd80ce459515d3eb6463fe3f",
+    "url": "/audio-hardware-archive/static/media/HCP.0216b4f4.WAV"
+  },
+  {
+    "revision": "ac12b1372fbd41e6928104e66994451d",
+    "url": "/audio-hardware-archive/static/media/HHC.ac12b137.WAV"
+  },
+  {
+    "revision": "61c8e958d77170f4e0917039e2f9365f",
+    "url": "/audio-hardware-archive/static/media/HHO.61c8e958.WAV"
+  },
+  {
+    "revision": "2994ae2a1ec7953b7407c76945246513",
+    "url": "/audio-hardware-archive/static/media/HT.2994ae2a.WAV"
+  },
+  {
+    "revision": "305d330518d1a784855455f6d3ca42d4",
+    "url": "/audio-hardware-archive/static/media/HighH.305d3305.wav"
+  },
+  {
+    "revision": "26a9c6631f0e52cc9c4cc7433652a80a",
+    "url": "/audio-hardware-archive/static/media/LowTom.26a9c663.wav"
+  },
+  {
+    "revision": "5b173302c24df2f1bf806dd0aa5d1e7e",
+    "url": "/audio-hardware-archive/static/media/OpenH.5b173302.wav"
+  },
+  {
+    "revision": "f8b1fe5fe760281087b5a142d7422dcc",
+    "url": "/audio-hardware-archive/static/media/RID.f8b1fe5f.WAV"
+  },
+  {
+    "revision": "876bc544369bae55a853318cd3f6d5d2",
+    "url": "/audio-hardware-archive/static/media/RIM.876bc544.WAV"
+  },
+  {
+    "revision": "0f67ca1a8ee9857853e7ef5db7fb2919",
+    "url": "/audio-hardware-archive/static/media/Ride.0f67ca1a.wav"
+  },
+  {
+    "revision": "ed0b71ae0cad40210869bd05b900282c",
+    "url": "/audio-hardware-archive/static/media/SD0.ed0b71ae.WAV"
+  },
+  {
+    "revision": "b3af4dc9412e462f060143500edbaa9a",
+    "url": "/audio-hardware-archive/static/media/Shaker.b3af4dc9.wav"
+  },
+  {
+    "revision": "0ea3d6079b9b286e2434033df60e2352",
+    "url": "/audio-hardware-archive/static/media/Snare.0ea3d607.wav"
+  },
+  {
+    "revision": "b78002d67d2b9d0b8271d6e54f811336",
+    "url": "/audio-hardware-archive/static/media/clap.b78002d6.wav"
+  },
+  {
+    "revision": "3a5c3647d551b47e09d9b4bf248b458b",
+    "url": "/audio-hardware-archive/static/media/hihat.3a5c3647.wav"
+  },
+  {
+    "revision": "ee5e489193d511be0a9fd07ef1adf878",
+    "url": "/audio-hardware-archive/static/media/kick.ee5e4891.wav"
+  },
+  {
+    "revision": "23e3e478a67fbb85f8411faa8a2db6c1",
+    "url": "/audio-hardware-archive/static/media/openhat.23e3e478.wav"
+  },
+  {
+    "revision": "aa5ed97c5f5d8d192e8a5874b3313cbd",
+    "url": "/audio-hardware-archive/static/media/ride.aa5ed97c.wav"
+  },
+  {
+    "revision": "ebbe45e246c02b8509e1ca4219773c37",
+    "url": "/audio-hardware-archive/static/media/rimshot.ebbe45e2.wav"
+  },
+  {
+    "revision": "b46adadb0aff6eea6ec391b7ca4f95aa",
+    "url": "/audio-hardware-archive/static/media/roland-tr-707.b46adadb.jpeg"
+  },
+  {
+    "revision": "0b05f7150c5c04bf408b49e0c27ff5bc",
+    "url": "/audio-hardware-archive/static/media/roland-tr-808.0b05f715.png"
+  },
+  {
+    "revision": "6fef40562614c6ecde70c37d221d18a6",
+    "url": "/audio-hardware-archive/static/media/roland-tr-909.6fef4056.jpeg"
+  },
+  {
+    "revision": "3099df3cfd54e45adfa13d4238be37b7",
+    "url": "/audio-hardware-archive/static/media/snare.3099df3c.wav"
+  },
+  {
+    "revision": "699c5537e6cdfab19641c3c6b9c222ad",
+    "url": "/audio-hardware-archive/static/media/synth-logo.699c5537.svg"
+  },
+  {
+    "revision": "c5ea106a6d97f84521b9f67a78631a54",
+    "url": "/audio-hardware-archive/static/media/tom1.c5ea106a.wav"
+  },
+  {
+    "revision": "0f36453b1fbc6fefb83ce0181ff5cd45",
+    "url": "/audio-hardware-archive/static/media/tom2.0f36453b.wav"
+  }
+]);
