cc3ebaa5c5c68cc3dd0b18b71dbe70c981288d22
Martin Foakes <foakes.m@gmail.com>
2020-04-03 18:42:30 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/audio-hardware-archive/precache-manifest.7f5e7430ea2fec10abbce2aaa9781f23.js"
+  "/audio-hardware-archive/precache-manifest.197ed18b4680d22e4a65bfb17082564d.js"
 );
 
 self.addEventListener('message', (event) => {
