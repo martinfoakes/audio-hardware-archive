0c4c99901666015e65a2923fabc4995534969b8b
Martin Foakes <foakes.m@gmail.com>
2020-04-29 10:52:34 +0200

    add hero image and section

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -4,10 +4,14 @@ import { HomeLink } from "../../Components";
 import RolandTr909 from "../../Img/device-images/roland-tr-909.jpeg";
 import RolandTr808 from "../../Img/device-images/roland-tr-808.png";
 import RolandTr707 from "../../Img/device-images/roland-tr-707.jpeg";
-import { UList } from "./styles/styles";
+import { Masthead, HeroText, UList } from "./styles/styles";
 
 const Home = () => (
   <>
+    <Masthead role="img" aria-label="Image Description">
+      <HeroText>{"Audio Hardware Archive"}</HeroText>
+    </Masthead>
+
     <UList>
       <HomeLink link="/tr-707" text="Roland TR-707" img={RolandTr707} alt="" />
       <HomeLink link="/tr-808" text="Roland TR-808" img={RolandTr808} alt="" />
--- src/Containers/Home/styles/styles.js
+++ src/Containers/Home/styles/styles.js
@@ -1,4 +1,35 @@
 import styled from "styled-components";
+import Hero from "../../../Img/hero.png";
+
+export const Masthead = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  width: 100%;
+  height: 100vh; /* if you don't want it to take up the full screen, reduce this number */
+  overflow: hidden;
+  background-size: cover;
+  background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0) 0%,
+      rgba(0, 0, 0, 0) 43%,
+      rgba(0, 0, 0, 0.65) 100%
+    ),
+    url(${Hero}) no-repeat center center scroll;
+`;
+
+export const HeroText = styled.h1`
+  font-style: normal;
+  font-weight: bold;
+  color: #eee;
+  font-size: 11vmin;
+  letter-spacing: 0.03em;
+  line-height: 1;
+  text-shadow: 1px 2px 4px rgba(0, 0, 0, 0.8);
+  margin-bottom: 30px;
+`;
 
 export const UList = styled.ul`
   list-style-type: none;
